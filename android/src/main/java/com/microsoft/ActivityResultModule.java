db903a9965652bd264b232ed04e592e155e7990c
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-07-12 10:30:25 -0300

    closes #1

--- android/src/main/java/com/microsoft/ActivityResultModule.java
+++ android/src/main/java/com/microsoft/ActivityResultModule.java
@@ -26,126 +26,126 @@ import javax.annotation.Nullable;
 
 public class ActivityResultModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
-  final SparseArray<Promise> mPromises;
+    final SparseArray<Promise> mPromises;
 
-  public ActivityResultModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-      mPromises = new SparseArray<>();
-  }
+    public ActivityResultModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        mPromises = new SparseArray<>();
+    }
 
-  @Override
-  public String getName() {
-      return "ActivityResult";
-  }
+    @Override
+    public String getName() {
+        return "ActivityResult";
+    }
 
-  @Nullable
-  @Override
-  public Map<String, Object> getConstants() {
-      HashMap<String, Object> constants = new HashMap<>();
-      constants.put("OK", Activity.RESULT_OK);
-      constants.put("CANCELED", Activity.RESULT_CANCELED);
-      return constants;
-  }
+    @Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        HashMap<String, Object> constants = new HashMap<>();
+        constants.put("OK", Activity.RESULT_OK);
+        constants.put("CANCELED", Activity.RESULT_CANCELED);
+        return constants;
+    }
 
-  @Override
-  public void initialize() {
-      super.initialize();
-      getReactApplicationContext().addActivityEventListener(this);
-  }
+    @Override
+    public void initialize() {
+        super.initialize();
+        getReactApplicationContext().addActivityEventListener(this);
+    }
 
-  @Override
-  public void onCatalystInstanceDestroy() {
-      super.onCatalystInstanceDestroy();
-      getReactApplicationContext().removeActivityEventListener(this);
-  }
+    @Override
+    public void onCatalystInstanceDestroy() {
+        super.onCatalystInstanceDestroy();
+        getReactApplicationContext().removeActivityEventListener(this);
+    }
 
-  @ReactMethod
-  public void startActivity(String action, ReadableMap data) {
-      Activity activity = getReactApplicationContext().getCurrentActivity();
-      Intent intent = new Intent(action);
-      intent.putExtras(Arguments.toBundle(data));
-      activity.startActivity(intent);
-  }
-  
-   @ReactMethod
-   public void startActivityWithPackageName(String packageName, String activityName, String data) {
-       ComponentName component = new ComponentName(packageName, activityName);
-       Activity activity = getReactApplicationContext().getCurrentActivity();
-       Intent intent = new Intent(Intent.ACTION_MAIN);
-       intent.addCategory(Intent.CATEGORY_LAUNCHER);
-       intent.setComponent(component);
-       intent.putExtra("data", data);
-       Log.d("data", data);
-       intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK |  Intent.FLAG_ACTIVITY_NEW_TASK);
-       activity.startActivity(intent);
-   }
+    @ReactMethod
+    public void startActivity(String action, ReadableMap data) {
+        Activity activity = getReactApplicationContext().getCurrentActivity();
+        Intent intent = new Intent(action);
+        intent.putExtras(Arguments.toBundle(data));
+        activity.startActivity(intent);
+    }
 
-   @ReactMethod
-   public void isIntentAvailable(String packageName, Promise promise) {
-       PackageManager pm = getReactApplicationContext().getPackageManager();
-       try {
-           pm.getPackageInfo(packageName, 0);
-           WritableMap map = new WritableNativeMap();
-           map.putString("result", "true");
-           promise.resolve(map);
-       } catch (PackageManager.NameNotFoundException e) {
-           promise.resolve(null);
-           return;
-       }
-   } 
+    @ReactMethod
+    public void startActivityWithPackageName(String packageName, String activityName, String data) {
+        ComponentName component = new ComponentName(packageName, activityName);
+        Activity activity = getReactApplicationContext().getCurrentActivity();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setComponent(component);
+        intent.putExtra("data", data);
+        Log.d("data", data);
+        intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK |  Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
 
-  @ReactMethod
-  public void startActivityForResult(int requestCode, String action, ReadableMap data, Promise promise) {
-      Activity activity = getReactApplicationContext().getCurrentActivity();
-      Intent intent = new Intent(action);
-      intent.putExtras(Arguments.toBundle(data));
-      activity.startActivityForResult(intent, requestCode);
-      mPromises.put(requestCode, promise);
-  }
+    @ReactMethod
+    public void isIntentAvailable(String packageName, Promise promise) {
+        PackageManager pm = getReactApplicationContext().getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, 0);
+            WritableMap map = new WritableNativeMap();
+            map.putString("result", "true");
+            promise.resolve(map);
+        } catch (PackageManager.NameNotFoundException e) {
+            promise.resolve(null);
+            return;
+        }
+    } 
 
-  @ReactMethod
-  public void resolveActivity(String action, Promise promise) {
-      Activity activity = getReactApplicationContext().getCurrentActivity();
-      Intent intent = new Intent(action);
-      ComponentName componentName = intent.resolveActivity(activity.getPackageManager());
-      if (componentName == null) {
-          promise.resolve(null);
-          return;
-      }
+    @ReactMethod
+    public void startActivityForResult(int requestCode, String action, ReadableMap data, Promise promise) {
+        Activity activity = getReactApplicationContext().getCurrentActivity();
+        Intent intent = new Intent(action);
+        intent.putExtras(Arguments.toBundle(data));
+        activity.startActivityForResult(intent, requestCode);
+        mPromises.put(requestCode, promise);
+    }
 
-      WritableMap map = new WritableNativeMap();
-      map.putString("class", componentName.getClassName());
-      map.putString("package", componentName.getPackageName());
-      promise.resolve(map);
-  }
+    @ReactMethod
+    public void resolveActivity(String action, Promise promise) {
+        Activity activity = getReactApplicationContext().getCurrentActivity();
+        Intent intent = new Intent(action);
+        ComponentName componentName = intent.resolveActivity(activity.getPackageManager());
+        if (componentName == null) {
+            promise.resolve(null);
+            return;
+        }
 
-  @ReactMethod
-  public void finish(int result, String action, ReadableMap map) {
-      Activity activity = getReactApplicationContext().getCurrentActivity();
-      Intent intent = new Intent(action);
-      intent.putExtras(Arguments.toBundle(map));
-      activity.setResult(result, intent);
-      activity.finish();
-  }
+        WritableMap map = new WritableNativeMap();
+        map.putString("class", componentName.getClassName());
+        map.putString("package", componentName.getPackageName());
+        promise.resolve(map);
+    }
 
-  @Override
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-      Promise promise = mPromises.get(requestCode);
-      if (resultCode != Activity.RESULT_OK){
-        promise.resolve(null);
-        return;
-      }
+    @ReactMethod
+    public void finish(int result, String action, ReadableMap map) {
+        Activity activity = getReactApplicationContext().getCurrentActivity();
+        Intent intent = new Intent(action);
+        intent.putExtras(Arguments.toBundle(map));
+        activity.setResult(result, intent);
+        activity.finish();
+    }
 
-      if (promise != null) {
-          WritableMap result = new WritableNativeMap();
-          result.putInt("resultCode", resultCode);
-          result.putMap("data", Arguments.makeNativeMap(data.getExtras()));
-          promise.resolve(result);
-      }
-  }
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        Promise promise = mPromises.get(requestCode);
+        if (resultCode != Activity.RESULT_OK){
+            promise.resolve(null);
+            return;
+        }
 
-  @Override
-  public void onNewIntent(Intent intent) {
-      /* Do nothing */
-  }
+        if (promise != null) {
+            WritableMap result = new WritableNativeMap();
+            result.putInt("resultCode", resultCode);
+            result.putMap("data", Arguments.makeNativeMap(data.getExtras()));
+            promise.resolve(result);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        /* Do nothing */
+    }
 }
